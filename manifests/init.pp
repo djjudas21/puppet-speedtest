3acd06118684d792e0574c30408047c9245153cb
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2014-11-12 13:27:25 +0000

    First pass at this module with basic functionality

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,54 @@
 # == Class: speedtest
-#
-# Full description of class speedtest here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'speedtest':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2014 Your name here, unless otherwise noted.
-#
-class speedtest {
 
+class speedtest (
+  $source,
+  $webroot = '/var/www/speedtest',
+  $vhost = $::fqdn,
+  $firewall = false,
+) {
 
+  # Install zip file from source
+  file { 'mini.zip':
+    source => $source,
+    path   => "${webroot}/mini.zip",
+    notify => Exec['unzip'],
+  }
+  
+  # Unzip the zip file
+  exec { 'unzip':
+    command     => 'unzip mini.zip',
+    cwd         => $webroot,
+    path        => '/usr/bin',
+    refreshonly => true,
+  }
+
+  # Create the webroot
+  file { $webroot:
+    ensure => directory,
+  }
+
+  # Symlink the version
+  file { "${webroot}/index.html":
+    ensure => 'link',
+    target => 'index-php.html',
+  }
+
+  # Install the dependencies
+  include apache
+  include apache::mod::php
+
+  # Define the apache server
+  apache::vhost { $vhost:
+    docroot  => $webroot,
+    priority => '25',
+  }
+
+  # Create a firewall exception
+  if ($firewall) {
+    firewall { '100-speedtest':
+      proto  => 'tcp',
+      dport  => '80',
+      action => 'accept',
+    }
+  }
 }
